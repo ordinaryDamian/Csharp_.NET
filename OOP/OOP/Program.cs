7fbaf4f86d703df8230bb85770176c13e8a7fbc7
ordinaryDamian <nadzadydam.14@gmail.com>
2023-03-30 12:20:17 +0200

    funky pridanie posledné

--- OOP/OOP/Program.cs
+++ OOP/OOP/Program.cs
@@ -1,18 +1,29 @@
-﻿
-class car
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP //nazov projektu
 {
-    int horsePower = 1000;
-    string color = "red";
-    bool fourWheels = true;
-    static void Main(string[] args)
+    internal class Program //class dost casto meno suboru
+    {
+        static void Main(string[] args)
     {   
-        car myObj = new car();
-        car myObj1 = new car();
+        Car myObj = new Car();
+        Car myObj1 = new Car();
         myObj1.color = "orange";
-        myObj.horsePower = 1500;
+        // int HPplus = int.Parse(Console.ReadLine());
+        // myObj.horsePower = myObj.horsePower + HPplus;
         Console.WriteLine(myObj.color);
         Console.WriteLine(myObj1.color);
         Console.WriteLine(myObj.horsePower);
         Console.WriteLine(myObj1.horsePower);
+        Console.WriteLine(myObj.fourWheels);
+        myObj1.fourWheels = false;
+        Console.WriteLine(myObj1.fourWheels == true ? "Auto ma 4 kolesá" : "Auto nemá 4 kolesá"); //uz pokracuje proceduralne hovori či je to true tak vypise prvu vec ak nie je true tak vypise druhu vec
+        Console.ReadKey();
+
+    }
     }
 }
---
+++ OOP/OOP/car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    internal class Car
+    {
+        public int horsePower = 1000;
+        public string color = "red";
+        public bool fourWheels = true;
+    }
+}
