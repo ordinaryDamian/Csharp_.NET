7223c8972c7b21e43933f32ffefe0f415a50763d
ordinaryDamian <nadzadydam.14@gmail.com>
2023-03-30 11:56:20 +0200

    OOP

--- OOP/OOP/Program.cs
+++ OOP/OOP/Program.cs
@@ -1,16 +1,18 @@
 ﻿
 class car
 {
+    int horsePower = 1000;
     string color = "red";
     bool fourWheels = true;
     static void Main(string[] args)
-
-    {
+    {   
         car myObj = new car();
         car myObj1 = new car();
         myObj1.color = "orange";
+        myObj.horsePower = 1500;
         Console.WriteLine(myObj.color);
         Console.WriteLine(myObj1.color);
-        Console.WriteLine(myObj1.fourWheels);
+        Console.WriteLine(myObj.horsePower);
+        Console.WriteLine(myObj1.horsePower);
     }
 }
